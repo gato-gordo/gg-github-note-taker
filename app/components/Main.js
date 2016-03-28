b6343e896f0a59035619f1a6a485a01f1e67c478
Ignacio Prado <ignacio@gatogordoweb.com>
2016-03-28 13:01:45 -0500

    ip - add react-router, index-route, and home component

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,15 +3,20 @@ var React = require('react');
 var Main = React.createClass({
    render: function(){
        return (
-           <div>
-                Hello World
-            </div>
+           <div className="main-container">
+               <nav className="navbar navbar-default" role="navigation">
+                    <div className="cols-sm-7-offset-2" style={{marginTop: 15}}>
+                        MENU
+                    </div>
+                </nav>
+                <div className="container">
+                    {this.props.children}
+               </div>
+           </div>
+
        );
    }
 });
 
 module.exports = Main;
 
-
-
-//ReactDOM.render(<Main />, document.getElementById('app'));
--- app/config/Routes.js
+++ app/config/Routes.js
@@ -3,7 +3,10 @@ var Main = require('../components/Main');
 var Home = require('../components/Home');
 var Router = require('react-router');
 var Route = Router.Route;
+var IndexRoute = Router.IndexRoute;
 
-module.export = (
-    <Route path="/" component={Main} ></Route>
+module.exports = (
+    <Route path="/" component={Main} >
+        <IndexRoute component={Home} />
+    </Route>
 );
