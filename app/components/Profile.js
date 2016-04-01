a900f1925432bf8cb27719eee7804806079cbcc9
Ignacio Prado <ignacio@gatogordoweb.com>
2016-04-01 10:03:53 -0500

    ip - refactor helpers to use es2015 features

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,7 +5,7 @@ var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 var ReactFireMixin = require('reactfire');
 var Firebase = require('firebase');
-var helpers = require('../utils/helpers');
+import getGithubInfo from '../utils/helpers';
 
 var Profile = React.createClass({
     mixins: [ReactFireMixin],
@@ -30,7 +30,7 @@ var Profile = React.createClass({
     init: function(username){
         var childRef = this.ref.child(username)
         this.bindAsArray(childRef, 'notes');
-        helpers.getGithubInfo(username)
+        getGithubInfo(username)
             .then(function(data){
                 this.setState({
                     bio: data.bio,
